e2c00e27916db4fbf8e0efbeddf8df1c23ee4951
ashokjr99 <ashoksharmajr@gmail.com>
2023-12-15 18:06:49 -0600

    check

--- index.js
+++ index.js
@@ -7,18 +7,48 @@ function ask(questionText) {
   });
 }
 
-start();
-
 async function start() {
-  console.log(
-    "Let's play a game where you (human) make up a number and I (computer) try to guess it."
-  );
-  let secretNumber = await ask(
-    "What is your secret number?\nI won't peek, I promise...\n"
-  );
-  console.log("You entered: " + secretNumber);
-  // Now try and complete the program.
-  process.exit();
-}
+  const min = 1;
+  const max = 100;
+  const answer = Math.floor(Math.random() * (max - min + 1));
 
-console.log("hhi");
+  let tries = 1;
+  let guess;
+  let running = true;
+
+  guess = await ask(`Guess a number between ${min} - ${max}`);
+  while (running === true) {
+    guess = Number(guess);
+
+    if (guess < min || guess > max) {
+      console.log(`Guess a number between 1-100 ONLY`);
+    } else {
+      attempts++;
+      if (guess < answer) {
+        console.log("Your answer is too low! Try again!");
+      } else if (guess > answer) {
+        console.log("Your answer is too high! Try again!");
+      } else {
+        console.log(
+          `The number was ${answer}. Correct! It took you ${tries}tries.`
+        );
+      }
+    }
+
+    running = false;
+  }
+
+  start();
+  // console.log(
+  //   "Let's play a game where you (human) make up a number and I (computer) try to guess it."
+  // );
+
+  // let secretNumber = await ask(
+  //   "What is your secret number?\nI won't peek, I promise...\n"
+  // );
+
+  // console.log("You entered: " + secretNumber);
+  // // Now try and complete the program.
+
+  // process.exit();
+}
