a7c2d28d8536c92e7035c138fff76e1bdd19db9c
ashokjr99 <ashoksharmajr@gmail.com>
2023-12-20 13:19:17 -0600

    done

--- index.js
+++ index.js
@@ -11,43 +11,45 @@ async function start() {
   let min = 1; // sets minimum
   let max = 100; // sets maximum
 
-  let tries = 0; // atempts it takes to guess the number
+  let tries = 1; // attempts it takes to guess the number
   let running = true; //game on OR game over
 
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
   );
   let secretNumber = await ask(
-    "What is your secret number?\nI won't peek, I promise..."
+    "What is your secret number?\nI won't peek, I promise..." // my variable for the secret number
   );
   console.log("You entered: " + secretNumber);
 
   while (running === true) {
-    let guess = Math.floor((max - min) / 2) + min; // the computer's guess at a number
-    let answer = await ask(`is your secret number ${guess}? (Y/N)`);
-    let panswer = "";
+    let guess = Math.floor((max - min) / 2) + min; // the computer's guess at a number variable
+    let answer = await ask(`is your secret number ${guess}? (Y/N)`); // the computer's guess at a number variable statement
+    let panswer = ""; // open quotes for string to pass through variable
 
     if (answer === "N") {
-      tries++;
+      tries++; // increments the variable of tries in my loop
       panswer = await ask("Is it higher (H), or lower (L)?"); //my answer to the computer's guess
-    }
-    if (panswer === "H") {
-      min = guess;
-    } else if (panswer === "L") {
-      max = guess;
-    } else {
+      if (panswer === "H") {
+        min = guess; // sets new min
+        if ((secretNumber = 100)) {
+          let guess = Math.floor((max - min) / 2 + min) + 2;
+        }
+      } else if (panswer === "L") {
+        max = guess; // sets new max
+      }
+    } else if (answer === "Y") {
       console.log(
         `The number was ${secretNumber}. Correct! It took you ${tries} tries.`
       );
-      again = await ask("Do you want to play again?. Yes(Y) or No(N)?");
+      again = await ask("Do you want to play again?. Yes(Y) or No(N)?"); // variable for play again question
       if (again === "Y") {
-        start();
+        start(); // restarts my function if user is wanting to play again
       } else {
-        running = false;
+        running = false; // exits my loop, but likely redundant
+        process.exit(); // exits my function if user is done
       }
     }
   }
 }
 start();
-
-// process.exit();
--- reverse_game.js
+++ reverse_game.js
@@ -25,29 +25,33 @@ async function start() {
   let running = true; //game on OR game over
 
   while (running === true) {
-    guess = await ask(`Guess a number between ${min} - ${max}`);
-    console.log(`My Guess is ${guess}`);
-    guess = Number(guess);
+    guess = await ask(`Guess a number between ${min} - ${max}`); // new bounds logged in statement
+    console.log(`My Guess is ${guess}`); // my guess logged in a statement
+    guess = Number(guess); //  making sure my guess remains a number value ONLY
 
     if (guess < min || guess > max) {
       console.log(`Guess a number between 1-100 ONLY`);
     } else {
-      tries++;
+      tries++; //will increment my tries variable
       if (guess < answer) {
         console.log("Your answer is too low! Try again!");
-        min = guess;
+        min = guess; // sets new min
       } else if (guess > answer) {
         console.log("Your answer is too high! Try again!");
-        max = guess;
+        max = guess; // sets new max
       } else {
         console.log(
           `The number was ${answer}. Correct! It took you ${tries} tries.`
         );
-        running = false;
+        again = await ask("Do you want to play again?. Yes(Y) or No(N)?"); // variable for play again question
+        if (again === "Y" || "y") {
+          start(); // restarts my function if user is wanting to play again
+        } else {
+          running = false; // exits my loop, but likely redundant
+          process.exit(); // exits my function if user is done
+        }
       }
     }
   }
 }
 start();
-
-// process.exit();
